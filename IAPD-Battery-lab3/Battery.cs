c8c742f4c330e85b1bf6db2eb9398b7622c78845
Samal Roman <samal_roma@mail.ru>
2017-10-14 22:52:40 +0300

    Added class for battery. Added WPF text blocks for power type, charge level and time.

---
+++ IAPD-Battery-lab3/Battery.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using IAPD_Battery_lab3;
+
+namespace IAPD_Battery_lab3
+{
+    class Battery
+    {
+        public string getChargeLevel()
+        {
+            WMIService wmi = new WMIService();
+            ManagementObjectCollection collection = wmi.getObject("Win32_Battery");
+            string result = "";
+            foreach (ManagementObject obj in collection)
+            {
+                result += obj["EstimatedChargeRemaining"];
+            }
+            return result;
+        }
+
+        public string getPowerType()
+        {
+            WMIService wmi = new WMIService();
+            ManagementObjectCollection collection = wmi.getObject("Win32_Battery");
+            string result = "";
+            foreach (ManagementObject obj in collection)
+            {
+                result += obj["BatteryStatus"];
+            }
+            if (result == "1" || result == "3" || result == "4" || result == "5" || result == "11")
+                return "Battery";
+            if (result == "2" || result == "6" || result == "7" || result == "8" || result == "9")
+                return "AC";
+            else
+                return "Unknown";
+        }
+    }
+}
