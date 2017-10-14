691572ea97dcd3b47bff368cfc6a54157dc8d726
Samal Roman <samal_roma@mail.ru>
2017-10-14 22:21:05 +0300

    Created resource file to store battery icon. Added xaml code for GUI.

--- IAPD-Battery-lab3/MainWindow.xaml.cs
+++ IAPD-Battery-lab3/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Management;
+using IAPD_Battery_lab3;
 
 namespace IAPD_Battery_lab3
 {
