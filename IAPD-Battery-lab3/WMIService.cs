cf7c94b084fa1f097205f16cb03c1bd7b05a8c64
Samal Roman <samal_roma@mail.ru>
2017-10-14 20:10:53 +0300

    Created class for WMI requests.

---
+++ IAPD-Battery-lab3/WMIService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace IAPD_Battery_lab3
+{
+    class WMIService
+    {
+        private ManagementScope scope;
+
+        public WMIService()
+        {
+            connectToWMI();
+        }
+
+        private void connectToWMI()
+        {
+            ManagementScope scope = new ManagementScope("\\root\\cimv2");
+            scope.Connect();
+        }
+
+        public ManagementObjectCollection getObject(string from)
+        {
+            ObjectQuery query = new ObjectQuery("SELECT * FROM " + from);
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+            return searcher.Get();
+        }
+    }
+}
